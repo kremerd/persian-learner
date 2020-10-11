63e181727d32fd967542af7df1c4b7570b0581a2
Dominik Kremer <dominik.kremer@gmx.de>
2020-10-11 13:13:06 +0200

    feat: add scss and bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
