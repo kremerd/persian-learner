492df99190c06bd327cbe8430c1ecaf5fac9ce09
Dominik Kremer <dominik.kremer@gmx.de>
2020-10-13 01:06:50 +0200

    feat: persist state to local storage

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from './components/App';
 import './index.scss';
 import reducer from './reducers';
 import * as serviceWorker from './serviceWorker';
 
 const store = configureStore({ reducer });
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,15 @@
 import { combineReducers } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import train from '../features/train/slice';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   train
 });
+
+const persistedReducer = persistReducer({
+  key: 'root',
+  storage
+}, rootReducer);
+
+export default persistedReducer;
