407c02afaecf8eca5cc2ffc0a0142f630733a034
Dominik Kremer <dominik.kremer@gmx.de>
2020-10-19 16:25:08 +0200

    Updates

---
+++ precache-manifest.2f910d447a27406f94a3f82fc166fe65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f27653205dec331bec3701f9cf1f9a",
+    "url": "/persian-learner/index.html"
+  },
+  {
+    "revision": "05a01092c768316bf5cb",
+    "url": "/persian-learner/static/css/main.90e6086d.chunk.css"
+  },
+  {
+    "revision": "a370e12e73f110c57e1c",
+    "url": "/persian-learner/static/js/2.d8bea1c5.chunk.js"
+  },
+  {
+    "revision": "d6787d3dcaa410e7408d87cc63c77792",
+    "url": "/persian-learner/static/js/2.d8bea1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a01092c768316bf5cb",
+    "url": "/persian-learner/static/js/main.7be35e85.chunk.js"
+  },
+  {
+    "revision": "79fe1b9d9aeebf129d5f",
+    "url": "/persian-learner/static/js/runtime-main.3bd8ebc1.js"
+  }
+]);
