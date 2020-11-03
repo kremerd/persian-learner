94a0708c4cfbc840cc78003379f737e6d270a498
Dominik Kremer <dominik.kremer@gmx.de>
2020-11-03 20:28:20 +0100

    fix: allow direct navigation to dictionary

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import dictionary from '../features/dictionary/routes';
-import trainer from '../features/trainer/routes';
+import { Redirect, Switch } from 'react-router-dom';
+import dictionaryRoutes from '../features/dictionary/routes';
+import trainerRoutes from '../features/trainer/routes';
 
-export default [
-  ...dictionary,
-  ...trainer,
-  <Redirect key="root"
-    from="/"
-    to="/trainer">
-  </Redirect>
-];
+export default (
+  <Switch>
+    {dictionaryRoutes},
+    {trainerRoutes},
+    <Redirect key="root" to="/trainer">
+    </Redirect>
+  </Switch>
+);
