3b34a12bf01ab64f4d382df89b730ced1b9d6632
Dominik Kremer <dominik.kremer@gmx.de>
2020-11-04 21:08:04 +0100

    feat: allow to clear search box

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -35,3 +35,9 @@ export const Plus = (): JSX.Element => (
     <path fillRule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
   </svg>
 );
+
+export const X = (): JSX.Element => (
+  <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+  </svg>
+);
--- src/features/dictionary/components/Dictionary/Dictionary.tsx
+++ src/features/dictionary/components/Dictionary/Dictionary.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { DictionaryEntry } from '../../model/dictionaryEntry';
 import { DictionaryFilter } from '../../model/dictionaryFilter';
 import { DictionaryAccordeon } from '../DictionaryAccordeon/DictionaryAccordeon';
+import { DictionarySearch } from '../DictionarySearch/DictionarySearch';
 
 export class Dictionary extends React.Component<{
   entries: DictionaryEntry[];
@@ -15,13 +16,11 @@ export class Dictionary extends React.Component<{
     return (
       <div>
         <h2>Wörterbuch</h2>
-        <input className="form-control"
-          type="text"
-          placeholder="Search dictionary..."
-          ref={this.searchRef}
-          value={this.props.filter?.searchTerm ?? ''}
-          onChange={(): void => this.updateSearchTerm()}
-        />
+        <DictionarySearch
+          currentSearch={this.props.filter?.searchTerm}
+          search={(s: string | null): void => this.props.search(s)}
+        >
+        </DictionarySearch>
         <table className="table dictionary">
           <tbody>
             {entries.map(e => (<DictionaryAccordeon key={e.id} entry={e}></DictionaryAccordeon>))}
