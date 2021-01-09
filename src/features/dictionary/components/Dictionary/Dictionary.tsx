2e27f6808110566224f91f67182f844f53812155
Dominik Kremer <dominik.kremer@gmx.de>
2021-01-09 21:14:41 +0100

    fix: fixate dictionary layout

--- src/features/dictionary/components/Dictionary/Dictionary.tsx
+++ src/features/dictionary/components/Dictionary/Dictionary.tsx
@@ -3,25 +3,24 @@ import { DictionaryEntry } from '../../model/dictionaryEntry';
 import { DictionaryFilter } from '../../model/dictionaryFilter';
 import { DictionaryAccordeon } from '../DictionaryAccordeon/DictionaryAccordeon';
 import { DictionarySearch } from '../DictionarySearch/DictionarySearch';
+import './Dictionary.scss';
 
 export class Dictionary extends React.Component<{
   entries: DictionaryEntry[];
   filter: Partial<DictionaryFilter>;
   search: (searchTerm: string | null) => void;
 }> {
-  private searchRef = React.createRef<HTMLInputElement>();
-
   render(): JSX.Element {
     const { entries } = this.props;
     return (
-      <div>
+      <div className="dictionary">
         <h2>Wörterbuch</h2>
         <DictionarySearch
           currentSearch={this.props.filter?.searchTerm}
           search={(s: string | null): void => this.props.search(s)}
         >
         </DictionarySearch>
-        <table className="table dictionary">
+        <table className="table">
           <tbody>
             {entries.map(e => (<DictionaryAccordeon key={e.id} entry={e}></DictionaryAccordeon>))}
           </tbody>
